d9a2a570698e83ec288fd30cd4d79b90b12746f3
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-06-28 22:34:32 +0100

    Updated the typescript setup

---
+++ backend-ts/server.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import products from "./products";
+import { IProduct } from "./models/models";
+
+const port = 5001;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running.");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product: IProduct | undefined = products.find(p => p._id === req.params.id);
+  if (product === undefined) res.send("product not found");
+  res.json(product);
+});
+
+app.listen(port, () => {
+  console.log(`server running on port: ${port}`);
+});
