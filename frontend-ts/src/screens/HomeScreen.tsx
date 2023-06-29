c64266021637ffa3618c846757f5a1fefe41acfc
Alex macbookAir <aleksandrmisnov@gmail.com>
2023-06-29 12:33:03 +0100

    changed the hardcoded products to use api calls instead

--- frontend-ts/src/screens/HomeScreen.tsx
+++ frontend-ts/src/screens/HomeScreen.tsx
@@ -1,14 +1,28 @@
+import { useEffect, useState } from "react";
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
+// import products from "../products";
+import { IProduct } from "../models/models";
 import Product from "../components/Product";
+import axios from "axios";
 
 // const HomeScreen: React.FC = () => {
 const HomeScreen = (): JSX.Element => {
+  const [products, setProducts] = useState<IProduct[]>();
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest products</h1>
       <Row>
-        {products.map(product => (
+        {products?.map(product => (
           <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
             <Product product={product} />
           </Col>
